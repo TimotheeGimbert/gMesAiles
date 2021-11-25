59f3480f2ad7ac0c1e32d87e8888b09bc16ea7b1
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-25 14:41:48 +0100

    fixes post email creation in DB

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,7 +5,16 @@ class EmailsController < ApplicationController
   end
 
   def create
-    redirect_to root_path
+    @email = Email.new(object: Faker::Ancient.god, body: Faker::Lorem.sentence(word_count: 11))
+    puts @email
+    if @email.save 
+      puts @email
+      redirect_to root_path
+    else
+      puts "issue"
+      flash.now[:danger] = 'Invalid email parameters !'
+      render 'new'
+    end
   end
 
 end
